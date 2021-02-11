12c800544dc0dc53e1ca5bf4b55a7db0fc0c9269
dani7beth <danielledona28@gmail.com>
2021-02-10 22:56:46 -0700

    added pictures

--- src/pages/Horoscope.js
+++ src/pages/Horoscope.js
@@ -1,24 +1,53 @@
-import { ButtonGroup } from "react-bootstrap";
+import styled from "styled-components";
+import aquarius from "../images/aquarius.png";
+import pisces from "../images/pisces.png";
+import aries from "../images/aries.png";
+import taurus from "../images/taurus.png";
 
-const Horoscope = () =>{
-  return(
+const Horoscope = () => {
+  return (
     <>
-    <h1>To get started select your sign</h1>
-    <div className="cardGroup">
-      <div>Aquarius</div>
-      <div>Pisces</div>
-      <div>Aries</div>
-      <div>Taurus</div>
-      <div>Gemini</div>
-      <div>Cancer</div>
-      <div>Leo</div>
-      <div>Virgo</div>
-      <div>Libra</div>
-      <div>Scorpio</div>
-      <div>Sagittarius</div>
-      <div>Capricorn</div>
-    </div>
+      <h1>To get started select your sign</h1>
+      <div className="cardGroup">
+        <Aquarius></Aquarius>
+        <Pisces></Pisces>
+        <Aries></Aries>
+        <Taurus></Taurus>
+        <div>Gemini</div>
+        <div>Cancer</div>
+        <div>Leo</div>
+        <div>Virgo</div>
+        <div>Libra</div>
+        <div>Scorpio</div>
+        <div>Sagittarius</div>
+        <div>Capricorn</div>
+      </div>
     </>
-  )
-}
+  );
+};
 export default Horoscope;
+
+export const Aquarius = styled.div`
+  background: url(${aquarius}) no-repeat;
+  background-size: contain;
+  width: 200px;
+  height: 400px;
+`;
+export const Pisces = styled.div`
+  background: url(${pisces}) no-repeat;
+  background-size: contain;
+  width: 200px;
+  height: 200px;
+`;
+export const Aries = styled.div`
+  background: url(${aries}) no-repeat;
+  background-size: contain;
+  width: 200px;
+  height: 400px;
+`;
+export const Taurus = styled.div`
+  background: url(${taurus}) no-repeat;
+  background-size: contain;
+  width: 200px;
+  height: 400px;
+`;
